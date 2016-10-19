81aea6c802a02c1d344a36806581d92d5ea5d5c3
GerdGames <trevorjancona@gmail.com>
2016-10-18 20:58:30 -0400

    Add files via upload

--- CowBro.ino
+++ CowBro.ino
@@ -6,42 +6,105 @@ ArduboyTunes arduTune;
 
 int x = 50;
 int y = 26;
+int bulletX, bulletY;
+int targetX, targetY;
 int frame = 0;
+int score = 0;
+String sound = "off";
+boolean targetActive;
+boolean front = true;
 boolean bulletActive = false;
+boolean game = false;
+boolean pause = false;
+boolean unpauseCooldown = false;
+
 
 void setup() 
 {
   // put your setup code here, to run once:
   ardu.begin();
-  ardu.setFrameRate(30);
+  ardu.setFrameRate(60);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-    if(ardu.nextFrame())
+  //to run the menu
+  if(!game)
+  {
+    //Display game title and menu options
+    ardu.clear();
+    ardu.setCursor(0, 0);
+    ardu.println("CowBro");
+    ardu.setCursor(0, 48);
+    ardu.println("Press A or B to start");
+    ardu.println("Sound: " + sound);
+    ardu.display();
+
+    if((ardu.pressed(A_BUTTON))||(ardu.pressed(B_BUTTON)))
+    {
+      game = true;
+    }
+  }
+  if(pause)
+  {
+    ardu.setCursor(0,8);
+    ardu.print("Paused A+B to resume");
+    ardu.display();
+    if(ardu.pressed(A_BUTTON) && ardu.pressed(B_BUTTON))
+    {
+      pause = false;
+      unpauseCooldown = true;
+    }
+    
+  }
+  //to run the game itself
+    if(game && !pause && ardu.nextFrame())
     {
       ardu.clear();
+
+      //to set target parameters
+      if(!targetActive)
+      {
+        targetActive = true;
+        ardu.initRandomSeed();
+        targetX = random(1, 119);
+        targetY = random(9, 55);
+      }
+      //to create targets
+      if(targetActive)
+      {
+        if (targetX < 64)
+        ardu.drawBitmap(targetX, targetY, targetLeft, 8, 8, WHITE);
+        else
+        ardu.drawBitmap(targetX, targetY, targetRight, 8, 8, WHITE);
+      }
+      //to perform hit detection
+      if((bulletX >= targetX)&&(bulletX <= targetX + 8)&&(bulletY >= targetY)&&(bulletY <= targetY + 8))
+      {
+        bulletActive = false;
+        targetActive = false;
+        score++;
+      }
       
       //to get movement input, to move Cowbro and change frames on his animation
       if((ardu.pressed(RIGHT_BUTTON))&&(x < 112))
       {
-        x += 2;
+        x += 1;
         frame += 1;
       }
       if((ardu.pressed(LEFT_BUTTON))&&(x > 0))
       {
-        x -= 2;
+        x -= 1;
         frame += 1;
       }
-      if((ardu.pressed(UP_BUTTON))&&(y > 0))
+      if((ardu.pressed(UP_BUTTON))&&(y > 8))
       {
-        y -= 2;
+        y -= 1;
         frame += 1;
       }
       if((ardu.pressed(DOWN_BUTTON))&&(y < 48))
       {
-        y += 2;
+        y += 1;
         frame += 1;
       }
       
@@ -49,35 +112,81 @@ void loop()
       if((ardu.pressed(B_BUTTON)) && (!bulletActive))
       {
         bulletActive = true;
+        front = true;
+        bulletX = x + 12;
+        bulletY = y + 9;
       }
+       if((ardu.pressed(A_BUTTON)) && (!bulletActive))
+      {
+        bulletActive = true;
+        front = false;
+        bulletX = x + 2;
+        bulletY = y + 9;
+      }
+      //to get inputs for pausing the game
+      if(ardu.pressed(A_BUTTON) && ardu.pressed(B_BUTTON) && !unpauseCooldown)
+      {
+        pause = true;
+      }
+      //to see if the game can be paused again
+      if(unpauseCooldown)
+      {
+        int count = 0;
+        while(count < 60)
+        {
+          count++;
+        }
+      }
+      //to create and move the bullet
       if(bulletActive)
       {
-        bullet(x + 12, y + 8);
+        if(front)
+        {
+          ardu.drawLine(bulletX, bulletY, bulletX + 4, bulletY, WHITE);
+          bulletX += 4;
+        }
+        else
+        {
+          ardu.drawLine(bulletX + 4, bulletY, bulletX, bulletY, WHITE);
+          bulletX -= 4;
+        }
+      }
+      if(bulletX > 128 || bulletX < -4)
+      {
+        bulletActive = false;
       }
       
-      //to animate the sprite
-      if(frame >= 2)
+      //to animate the cowboy sprite
+      if (front)
       {
-        ardu.drawBitmap(x, y, cowBro, 16, 16, WHITE);
+        if(frame > 4)
+        {
+          ardu.drawBitmap(x, y, cowBro, 16, 16, WHITE);
+        }
+        else
+        {
+          ardu.drawBitmap(x, y, cowBroStep, 16, 16, WHITE);
+        }
       }
       else
       {
-        ardu.drawBitmap(x, y, cowBroStep, 16, 16, WHITE);
+        if(frame > 4)
+        {
+          ardu.drawBitmap(x, y, cowBroFlip, 16, 16, WHITE);
+        }
+        else
+        {
+          ardu.drawBitmap(x, y, cowBroStepFlip, 16, 16, WHITE);
+        }
       }
-      if(frame >= 4)
+      if(frame > 8)
       {
         frame = 0;
       }
+      ardu.setCursor(0, 0);
+      ardu.print("Score ");
+      ardu.print(score);
       ardu.display();
     }
-}
-//The purpose of this method is to create and move bullet objects
-void bullet(int bulletX, int bulletY)
-{
-  bulletActive = false;
-  for(int count = 0; count < 128; count += 2)
-  {
-    ardu.drawBitmap(bulletX + count, bulletY, bulletSprite, 8, 3, WHITE);
-  }
 }
 
